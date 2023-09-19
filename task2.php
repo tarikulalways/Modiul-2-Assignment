0a4271cbf46a082f02fe80103c79d5b9e6978c9b
Md Tarikul Islam <120883932+tarikulalways@users.noreply.github.com>
2023-09-19 10:06:59 +0600

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,20 +1,4 @@
 <?php
-function s(){
-    for ($i = 1; $i <= 50; $i++) {
-        // Check if $i is a multiple of 5
-        if ($i % 5 == 0) {
-            // Skip this iteration and continue to the next number
-            continue;
-        }
-        return $i." ";
-    }
-}
-
-
-?>
-
-
-
 <div class="row">
     <div class="col-md-6 m-auto mb-3">
         <div class="alert alert-success">Task 2: Skip Multiples of 5</div>
